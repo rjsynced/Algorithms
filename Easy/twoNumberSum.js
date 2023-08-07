7509a115f9f42d561ea8bf2171505601b8bb344e
Judah Mendoza <judahramses@gmail.com>
2023-08-07 10:09:25 -0700

    8/7/23 morning algo

---
+++ Easy/twoNumberSum.js
@@ -0,0 +1,45 @@
+// write a function that takes in a non-empty array of distinct integers and an integer represeting a target sum. If any two numbers in the input aray sum up to the target sum, the function should return the min an aray, in any order. If no two numbers sum up to the taret sum, the function should return an empty array.
+
+// not that the target sum has to be obtained by suming two different integers in the array; you can't add a single integer to itself in order to obtain the target sum.
+
+// you can assume that there will be most pair of numbers summing to the target sum.
+
+array = [3, 5, -4, 8, 11, 1, -1, 6]
+
+targetSum = 10
+
+// sample output should be [-1,11]
+
+// double for-loop solution
+function twoNumberSum(array, targetSum) {
+    let result = []
+    for(let i = 0; i < array.length; i++) {
+        for(let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] == targetSum) {
+                result.push(array[j], array[i]);
+            }
+        }
+    }
+    return result;
+}
+
+console.log(twoNumberSum(array, targetSum))
+
+// O(n) Time | O(n) Space Solution
+function twoNumberSum1(array, targetSum) {
+    const seen = new Set();
+
+    for(let num of array) {
+        // complement is the number that will sum up to target when added to the current number we are considering
+        const complement = targetSum - num;
+        // if we've seen the complement before then we have a pair fo number summing up to the target
+        if(seen.has(complement)) {
+            return [num, complement];
+        }
+        // mark the number as seen
+        seen.add(num);
+    }
+
+    return []
+}
+console.log(twoNumberSum1(array, targetSum))
