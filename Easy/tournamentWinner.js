beefacf0e09cab259cd09d4b394064288a855a4e
Judah Mendoza <judahramses@gmail.com>
2023-08-23 07:46:07 -0700

    8/23/23 algo

--- Easy/tournamentWinner.js
+++ Easy/tournamentWinner.js
@@ -13,9 +13,31 @@ results = [0, 0, 1]
 // C# - 3 points
 // Python - 6 points
 
+const HOME_TEAM_WON = 1;
+// solution 1 O(n)
 function tournamentWinner(competitions, results) {
-    // write code here
-    return '';
+    let currentBestTeam = '';
+    const scores = {[currentBestTeam]: 0};
+
+    for (let i = 0; i < competitions.length; i++) {
+        let result = results[i];
+        const [homeTeam, awayTeam] = competitions[i];
+
+        const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+        updateScores(winningTeam, 3, scores);
+
+        if (scores[winningTeam] > scores[currentBestTeam]) {
+            currentBestTeam = winningTeam;
+        }
+    }
+    return currentBestTeam;
+}
+
+function updateScores(team, points, scores) {
+    if(!(team in scores)) scores[team] = 0;
+
+    scores[team] += points;
 }
 
 console.log(tournamentWinner(competitions, results))
